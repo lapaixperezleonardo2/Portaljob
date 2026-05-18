8f6d57d9518ca8914ec744ebdeea98ce0a437f27
Darkness <darkness@gmail.com>
2026-05-18 00:03:26 -0400

    fix imports

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,9 @@ import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=os.getenv("DB_PORT")
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=os.getenv("MYSQLPORT")
     )
